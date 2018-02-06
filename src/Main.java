da387d6906c6502c08dce912f500c885705448d6
Michael <m.altair@live.com>
2018-02-06 12:02:25 -0400

    Developed the user input, with basic error handling for the support and confidence values

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,50 @@
+import java.util.Scanner;
 
 public class Main {
 
+	private static String filename;
+	private static double support;
+	private static double confidence;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Initial User Input - filename
+		System.out.print("Welcome to Apriori Miner!\nPlease enter data file's name: ");
+		Scanner in = new Scanner(System.in);
+		filename = in.nextLine();
+		
+		// Initial User Input - support
+		int reloop;
+		do {
+			reloop = 0;
+			System.out.print("Please enter support percentage: ");
+			in = new Scanner(System.in);
+			try {
+				support = in.nextDouble();
+			} catch(Exception e) {
+				System.out.println("Incorrect format");
+				reloop++;
+			}
+		} while(reloop != 0);
 
+		
+		// Initial User Input - confidence	
+		do {
+			reloop = 0;
+			System.out.print("Please enter confidence percentage: ");
+			in = new Scanner(System.in);
+			try {
+				confidence = in.nextDouble();
+			} catch(Exception e) {
+				System.out.println("Incorrect format");
+				reloop++;
+			}
+		} while(reloop != 0);
+
+		// Just closing the scanner
+		in.close();
+		
+		// TODO: Just for testing user input
+		System.out.println("You entered...\nfilename: " + filename + "\nsupport: " + support + "\nconfidence: " + confidence);
 	}
 
 }
