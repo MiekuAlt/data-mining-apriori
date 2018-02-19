5333dd78de2f531e8be6c3817ec17773d5784b74
Michael <m.altair@live.com>
2018-02-19 15:36:55 -0400

    It is now properly pruning the data, and handling multiple expansions

--- src/Apriori.java
+++ src/Apriori.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
@@ -9,42 +11,80 @@ public final class Apriori {
 	private static List<List<String>> inputData;
 	private static double minSupport, minConfidence;
 	
-	private static double numUniques; // This is the number of unique items (Useful since we are doing support based on percent)
-	
 	public static List<String> runApriori(List<List<String>> data, double supportInput, double confidenceInput) {
 		inputData = data;
 		minSupport = supportInput;
 		minConfidence = confidenceInput;
 		
+		List<List<String>> curDataSets;
 		List<Item> uniqueItems;
 		List<KeyValue> candTable;
 		List<KeyValue> freqTable;
 		// Find all the unique itemSets in the data
 		uniqueItems = findUniquesInData(data);
-		numUniques = uniqueItems.size();
 		
 		candTable = buildFirstCand(uniqueItems);
 		
 		freqTable = buildFreq(candTable);
 		
-		uniqueItems = expandItemSet(freqTable, 2);
+		curDataSets = expandItemSet(freqTable, 2);
+		candTable = buildCand(curDataSets);
+		freqTable = buildFreq(candTable);
+		
+		curDataSets = expandItemSet(freqTable, 3);
+		candTable = buildCand(curDataSets);
+		freqTable = buildFreq(candTable);
+		
+		curDataSets = expandItemSet(freqTable, 4);
+		candTable = buildCand(curDataSets);
+		freqTable = buildFreq(candTable);
+		
+		System.out.println("Test Freq: ");
+		printTable(freqTable);
 		
 		//TODO: Remove this dummy output
 		List<String> dummyRules = Arrays.asList("(Support=0.29, Confidence=1.00) { outlook=overcast } ----> { PlayTennis=P }", "(Support=0.29, Confidence=0.67) { temperature=mild } ----> { Humidity=high }");
 		return dummyRules;
 	}
 	
-	// Expands the itemSets to the nth
-	private static List<Item> expandItemSet(List<KeyValue> table, int n) {
+	// Expands the itemSets to the nth based on an example from https://stackoverflow.com/questions/5162254/all-possible-combinations-of-an-array
+	private static List<List<String>> expandItemSet(List<KeyValue> table, int n) {
 		
 		List<String> curData = convertTableToData(table);
-		List<Item> expandedItemSets = new ArrayList<Item>();
-		
-		
-		return null;
+		List<List<String>> expandSet = new LinkedList<List<String>>();
+
+	    expandSet.addAll(combination(curData, n));
+	    System.out.println("Expanded: " + expandSet);
+		return expandSet;
 	}
 	
-	// Converts a table into the same datatype this is read on input
+	// This is based on an example from https://stackoverflow.com/questions/5162254/all-possible-combinations-of-an-array
+	public static <T> List<List<T>> combination(List<T> values, int size) {
+
+	    if (0 == size) {
+	        return Collections.singletonList(Collections.<T> emptyList());
+	    }
+
+	    if (values.isEmpty()) {
+	        return Collections.emptyList();
+	    }
+
+	    List<List<T>> combination = new LinkedList<List<T>>();
+	    T actual = values.iterator().next();
+	    List<T> subSet = new LinkedList<T>(values);
+	    subSet.remove(actual);
+	    List<List<T>> subSetCombination = combination(subSet, size - 1);
+
+	    for (List<T> set : subSetCombination) {
+	        List<T> newSet = new LinkedList<T>(set);
+	        newSet.add(0, actual);
+	        combination.add(newSet);
+	    }
+
+	    combination.addAll(combination(subSet, size));
+	    return combination;
+	}
+	// Converts a table into the same dataType this is read on input
 	private static List<String> convertTableToData(List<KeyValue> table) {
 		
 		List<String> newData = new ArrayList<String>();
@@ -52,35 +92,31 @@ public final class Apriori {
 		for(int r = 0; r < table.size(); r++) {
 			for(int c = 0; c < table.get(r).itemSet.size(); c++) {
 				newData.add(table.get(r).itemSet.get(c).value);
-				System.out.print(newData.get(r));
 			}
-			System.out.println("");
-		}
+		}	
 		
-		return newData;
+		return removeDups(newData);
 	}
 	
 	// Calculates the support value on an itemSet
 	private static double calcSupport(List<Item> itemsChecked) {
 		int numSupport = 0;
 
-		for(int r = 0; r < inputData.size(); r++) {
-			int amount = itemsChecked.size();
+		for(int r = 0; r < inputData.size(); r++ ) {
+			int numCheck = itemsChecked.size();
 			for(int i = 0; i < itemsChecked.size(); i++) {
-				Item curItem = itemsChecked.get(i);
-				// Ensures that all the items in the set fit
-				for(int c = 0; c < inputData.get(c).size() - 1; c++) {
-					if(inputData.get(r).get(c).equals(curItem.value)) {
-						amount--;
+				for(int c = 0; c < inputData.get(r).size(); c++) {
+					if(itemsChecked.get(i).value.equals(inputData.get(r).get(c))) {
+						numCheck--;
 					}
 				}
+				if(numCheck == 0) {
+					numSupport++;
+				}
 			}
-			// If everything matches, increase support
-			if(amount == 0) 
-				numSupport++;
 		}
 		
-		return numSupport / numUniques;
+		return numSupport / ((double)inputData.size());
 	}
 	
 	// Builds the frequency table
@@ -98,7 +134,24 @@ public final class Apriori {
 		return cand;
 	}
 	
-	// Builds a candidate table
+	// Builds the typical candidate table
+	private static List<KeyValue> buildCand(List<List<String>> itemSets) {
+		List<KeyValue> canTable = new ArrayList<KeyValue>();
+		
+		// Populating the items
+		for(int i = 0; i < itemSets.size(); i++) {
+			List<Item> entry = new ArrayList<Item>();
+			for(int j = 0; j < itemSets.get(i).size(); j++) {
+				entry.add(new Item(itemSets.get(i).get(j)));
+			}
+			canTable.add(new KeyValue(entry, calcSupport(entry)));
+		}
+		System.out.println("Can Table: ");
+		printTable(canTable);
+		return canTable;
+	}
+	
+	// Builds the first candidate table
 	private static List<KeyValue> buildFirstCand(List<Item> itemSets) {
 		List<KeyValue> canTable = new ArrayList<KeyValue>();
 		
@@ -108,7 +161,7 @@ public final class Apriori {
 			entry.add(itemSets.get(i));
  			canTable.add(new KeyValue(entry, calcSupport(entry)));
 		}
-		
+		System.out.println("Cand Table test");
 		printTable(canTable);
 		
 		return canTable;
@@ -125,12 +178,9 @@ public final class Apriori {
 			}
 		}
 		
-		// This is derived from an example here https://stackoverflow.com/questions/203984/how-do-i-remove-repeated-elements-from-arraylist
-		Set<String> hs = new HashSet<>();
-		hs.addAll(allItems);
-		List<String> uniques = new ArrayList<String>();
-		uniques.addAll(hs);
-		
+		// Removing duplicates
+		List<String> uniques = removeDups(allItems);
+
 		List<Item> uniqueItems = new ArrayList<Item>();
 		for(int i = 0; i < uniques.size(); i++) {
 			uniqueItems.add(new Item(uniques.get(i)));
@@ -149,12 +199,24 @@ public final class Apriori {
 		System.out.println("Table");
 		for(int r = 0; r < tableInfo.size(); r++) {
 			for(int c = 0; c < tableInfo.get(r).itemSet.size(); c++) {
-				System.out.print("| ");
 				System.out.print(tableInfo.get(r).itemSet.get(c).value + "\t| ");
-				System.out.println(tableInfo.get(r).support + " |");
 			}
+			System.out.print(tableInfo.get(r).support + " |");
+			System.out.println("");
 		}
 	}
+	
+	// Removes duplicates from a list of strings
+	private static List<String> removeDups(List<String> withDups) {
+		// This is derived from an example here https://stackoverflow.com/questions/203984/how-do-i-remove-repeated-elements-from-arraylist
+		Set<String> hs = new HashSet<>();
+		hs.addAll(withDups);
+		List<String> uniques = new ArrayList<String>();
+		uniques.addAll(hs);
+		
+		return uniques;
+	}
+	
 } // end of the Apriori class
 
 
